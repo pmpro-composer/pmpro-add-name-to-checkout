afa2e09475271216fc8f9a835109568cab763670
Tien <nhantien2806@gmail.com>
2020-04-20 19:01:28 -0400

    updated to v0.4

---
+++ pmpro-add-name-to-checkout.php
@@ -0,0 +1,169 @@
+<?php
+/*
+Plugin Name: Paid Memberships Pro - Add Name to Checkout Add On
+Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-add-name-to-checkout/
+Description: Adds first and last name fields to the user account section at checkout for Paid Memberships Pro.
+Version: .4
+Text Domain: pmpro-add-name-to-checkout
+Domain Path: /languages
+Author: Stranger Studios
+Author URI: http://www.strangerstudios.com
+*/
+
+/**
+ * Add the fields to the form.
+ */
+function pmproan2c_pmpro_checkout_after_password() {
+	global $current_user;
+	
+	if ( ! empty( $_REQUEST['first_name'] ) ) {
+		$first_name = sanitize_text_field( $_REQUEST['first_name'] );
+	} elseif ( ! empty( $_SESSION['first_name'] ) ) {
+		$first_name = sanitize_text_field( $_SESSION['first_name'] );
+	} elseif ( is_user_logged_in() ) {
+		$first_name = $current_user->first_name;
+	} else {
+		$first_name = '';
+	}
+
+	if ( ! empty( $_REQUEST['last_name'] ) ) {
+		$last_name = sanitize_text_field( $_REQUEST['last_name'] );
+	} elseif ( ! empty( $_SESSION['last_name'] ) ) {
+		$last_name = sanitize_text_field( $_SESSION['last_name'] );
+	} elseif ( is_user_logged_in() ) {
+		$last_name = $current_user->last_name;
+	} else {
+		$last_name = '';
+	}
+	?>
+	<div class="pmpro_checkout-field pmpro_checkout-field-firstname">
+		<label for="first_name"><?php _e( 'First Name', 'pmpro' ); ?></label>
+		<input id="first_name" name="first_name" type="text" class="input pmpro_required <?php echo pmpro_getClassForField( 'first_name' ); ?>" size="30" value="<?php echo esc_attr( $first_name ); ?>" />
+	</div>
+	<div class="pmpro_checkout-field pmpro_checkout-field-lastname">
+		<label for="last_name"><?php _e( 'Last Name', 'pmpro' ); ?></label>
+		<input id="last_name" name="last_name" type="text" class="input pmpro_required <?php echo pmpro_getClassForField( 'last_name' ); ?>" size="30" value="<?php echo esc_attr( $last_name ); ?>" />
+	</div> 
+	<?php
+}
+add_action( 'pmpro_checkout_after_password', 'pmproan2c_pmpro_checkout_after_password' );
+
+/**
+ * If the user is logged in, we still want to show our version of the account info section.
+ */
+function pmproan2c_account_info_when_logged_in() {
+	global $current_user;
+	
+	if ( ! is_user_logged_in() ) {
+		return;
+	}	
+	?>
+	<hr />
+	<div id="pmpro_user_fields" class="pmpro_checkout">
+		<h3>
+			<span class="pmpro_checkout-h3-name"><?php _e('Account Information', 'pmpro-add-name-to-checkout' );?></span>			
+		</h3>
+		<div class="pmpro_checkout-fields">
+			<?php pmproan2c_pmpro_checkout_after_password(); ?>
+		</div>
+	</div>
+	<?php
+}
+add_action( 'pmpro_checkout_after_pricing_fields', 'pmproan2c_account_info_when_logged_in' );
+
+/**
+ * Require the fields.
+ */
+function pmproan2c_pmpro_registration_checks() {
+	global $pmpro_msg, $pmpro_msgt, $current_user, $pmpro_error_fields;
+	if ( ! empty( $_REQUEST['first_name'] ) ) {
+		$first_name = trim( sanitize_text_field( $_REQUEST['first_name'] ) );
+	} elseif ( ! empty( $_SESSION['first_name'] ) ) {
+		$first_name = trim( sanitize_text_field( $_SESSION['first_name'] ) );
+	} elseif ( is_user_logged_in() ) {
+		$first_name = $current_user->first_name;
+	} else {
+		$first_name = '';
+	}
+
+	if ( ! empty( $_REQUEST['last_name'] ) ) {
+		$last_name = trim( sanitize_text_field( $_REQUEST['last_name'] ) );
+	} elseif ( ! empty( $_SESSION['last_name'] ) ) {
+		$last_name = trim( sanitize_text_field( $_SESSION['last_name'] ) );
+	} elseif ( is_user_logged_in() ) {
+		$last_name = $current_user->last_name;
+	} else {
+		$last_name = '';
+	}
+
+	if ( $first_name && $last_name || $current_user->ID ) {
+		//all good
+		return true;
+	} else {
+		$pmpro_msg  = __( 'The first and last name fields are required.', 'pmpro-add-name-to-checkout' );
+		$pmpro_msgt = 'pmpro_error';
+		if ( ! $first_name ) {
+			$pmpro_error_fields[] = 'first_name';
+		}
+		if ( ! $last_name ) {
+			$pmpro_error_fields[] = 'last_name';
+		}
+		return false;
+	}
+}
+add_filter( 'pmpro_registration_checks', 'pmproan2c_pmpro_registration_checks' );
+
+/**
+ * Update the user after checkout.
+ */
+function pmproan2c_update_first_and_last_name_after_checkout( $user_id ) {
+	global $current_user;
+
+	if ( ! empty( $_REQUEST['first_name'] ) ) {
+		$first_name = trim( sanitize_text_field( $_REQUEST['first_name'] ) );
+	} elseif ( ! empty( $_SESSION['first_name'] ) ) {
+		$first_name = trim( sanitize_text_field( $_SESSION['first_name'] ) );
+	} elseif ( is_user_logged_in() ) {
+		$first_name = $current_user->first_name;
+	} else {
+		$first_name = '';
+	}
+
+	if ( ! empty( $_REQUEST['last_name'] ) ) {
+		$last_name = trim( sanitize_text_field( $_REQUEST['last_name'] ) );
+	} elseif ( ! empty( $_SESSION['last_name'] ) ) {
+		$last_name = trim( sanitize_text_field( $_SESSION['last_name'] ) );
+	} elseif ( is_user_logged_in() ) {
+		$last_name = $current_user->last_name;
+	} else {
+		$last_name = '';
+	}
+
+	update_user_meta( $user_id, 'first_name', $first_name );
+	update_user_meta( $user_id, 'last_name', $last_name );
+}
+add_action( 'pmpro_after_checkout', 'pmproan2c_update_first_and_last_name_after_checkout' );
+
+/**
+ * Save our added fields in session while the user goes off to PayPal/etc
+ */
+function pmproan2c_pmpro_paypalexpress_session_vars() {
+	$_SESSION['first_name'] = trim( sanitize_text_field( $_REQUEST['first_name'] ) );
+	$_SESSION['last_name']  = trim( sanitize_text_field( $_REQUEST['last_name'] ) );
+}
+add_action( 'pmpro_paypalexpress_session_vars', 'pmproan2c_pmpro_paypalexpress_session_vars' );
+add_action( 'pmpro_before_send_to_twocheckout', 'pmproan2c_pmpro_paypalexpress_session_vars' );
+
+/**
+ * Add links to the plugin row meta
+ */
+function pmproan2c_plugin_row_meta( $links, $file ) {
+	if ( strpos( $file, 'pmpro-add-name-to-checkout.php' ) !== false ) {
+		$new_links = array(
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro-add-name-to-checkout' ) ) . '">' . __( 'Support', 'pmpro-add-name-to-checkout' ) . '</a>',
+		);
+		$links     = array_merge( $links, $new_links );
+	}
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'pmproan2c_plugin_row_meta', 10, 2 );
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Paid Memberships Pro - Add Name to Checkout Add On ===
+Contributors: strangerstudios
+Tags: paid memberships pro, pmpro, first name, fname, last name, lname, names
+Requires at least: 3.5
+Tested up to: 5.1.1
+Stable tag: .4
+
+Adds first and last name fields to the Paid Memberships Pro checkout page.
+
+== Description ==
+
+Adds first and last name fields to the Paid Memberships Pro checkout page.
+
+== Installation ==
+
+= Download, Install and Activate! =
+1. Upload the `pmpro-add-name-to-checkout` directory to the `/wp-content/plugins/` directory of your site.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= I found a bug in the plugin. =
+
+Please post it in the issues section of GitHub and we'll fix it as soon as we can. Thanks for helping. https://github.com/strangerstudios/pmpro-add-name-to-checkout/issues
+
+= I need help installing, configuring, or customizing the plugin. =
+
+Please visit our premium support site at http://www.paidmembershipspro.com for more documentation and our support forums.
+
+== Changelog ==
+= .4 =
+* SECURITY: Properly sanitizing and escaping user input.
+* BUG FIX/ENHANCEMENT: Using pmpro_getClassForField() to set class attributes on field tags. (Thanks, Ted Barnett)
+* BUG FIX/ENHANCEMENT: Added pmpro_checkout-field/etc classes to wrapping divs for field to match the core PMPro checkout fields and allow for easier styling. (Thanks, Mark Bloomfield)
+* BUG FIX/ENHANCEMENT: Using trim() when checking name fields to disallow blank names.
+* ENHANCEMENT: Make add-on translation ready. (Thanks, Thomas Sjolshagen)
+* ENHANCEMENT: WordPress coding standards.
+* FEATURE: Now showing the User Information section with name fields if a user is already logged in at checkout.
+
+= .3.1 =
+* BUG: Fixed error when PHP short tags is disabled.
+* BUG: Fixed some warnings.
+
+= .3 =
+* BUG: Fixed issue where first and last name were being "blanked out" when logged in users checked out again.
+
+= .2 =
+* Fixed some warnings. Changed priority on pmpro_after_checkout hook to 5 so name meta fields are setup before most other plugins will try to hook.
+* Now also checking if an existing user is logged in with an existing first/last name before potentially overwriting the first/last name on a second checkout.
+
+= .1 =
+* Initial commit.
